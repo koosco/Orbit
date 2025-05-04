74fdb24593340552f24943d3c1ebb787213d826b
Koo <koosco136@gmail.com>
2025-05-05 07:21:02 +0900

    refactor: SRP를 지키도록 Schedule Delete API 분리

--- src/main/java/com/groom/orbit/schedule/controller/ScheduleDeleteController.java
+++ src/main/java/com/groom/orbit/schedule/controller/ScheduleDeleteController.java
@@ -1,30 +1,23 @@
 package com.groom.orbit.schedule.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.groom.orbit.common.annotation.AuthMember;
 import com.groom.orbit.common.dto.CommonSuccessDto;
 import com.groom.orbit.common.dto.ResponseDto;
 import com.groom.orbit.schedule.app.ScheduleCommandService;
-import com.groom.orbit.schedule.app.dto.ScheduleRequestDto;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/schedule")
-public class ScheduleCommandController {
+public class ScheduleDeleteController {
 
   private final ScheduleCommandService scheduleCommandService;
-
-  @PatchMapping("/{scheduleId}")
-  public ResponseDto<CommonSuccessDto> updateSchedule(
-      @AuthMember Long memberId,
-      @PathVariable Long scheduleId,
-      @RequestBody ScheduleRequestDto scheduleRequestDto) {
-    return ResponseDto.ok(
-        scheduleCommandService.updateSchedule(memberId, scheduleId, scheduleRequestDto));
-  }
 
   @DeleteMapping("/{scheduleId}")
   public ResponseDto<CommonSuccessDto> deleteSchedule(
