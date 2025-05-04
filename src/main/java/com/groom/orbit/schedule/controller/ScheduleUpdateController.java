a5b3160e3496e24a3e8e7335c32114abe0667c36
Koo <koosco136@gmail.com>
2025-05-05 07:20:15 +0900

    refactor: SRP를 지키도록 Schedule Update API 분리

---
+++ src/main/java/com/groom/orbit/schedule/controller/ScheduleUpdateController.java
@@ -0,0 +1,28 @@
+package com.groom.orbit.schedule.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.groom.orbit.common.annotation.AuthMember;
+import com.groom.orbit.common.dto.CommonSuccessDto;
+import com.groom.orbit.common.dto.ResponseDto;
+import com.groom.orbit.schedule.app.ScheduleCommandService;
+import com.groom.orbit.schedule.app.dto.ScheduleRequestDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/schedule")
+public class ScheduleUpdateController {
+
+  private final ScheduleCommandService scheduleCommandService;
+
+  @PatchMapping("/{scheduleId}")
+  public ResponseDto<CommonSuccessDto> updateSchedule(
+      @AuthMember Long memberId,
+      @PathVariable Long scheduleId,
+      @RequestBody ScheduleRequestDto scheduleRequestDto) {
+    return ResponseDto.ok(
+        scheduleCommandService.updateSchedule(memberId, scheduleId, scheduleRequestDto));
+  }
+}
