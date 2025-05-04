513b33ec87a7b087ab10a46104dc174e01b247ac
Koo <koosco136@gmail.com>
2025-05-05 06:45:38 +0900

    refactor: SRP에 맞게 OAuth Controller 분리

--- src/main/java/com/groom/orbit/member/auth/controller/OAuthController.java
+++ src/main/java/com/groom/orbit/member/auth/controller/OAuthController.java
@@ -7,20 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.groom.orbit.common.exception.BaseResponse;
 import com.groom.orbit.member.auth.app.AuthService;
-import com.groom.orbit.member.auth.app.kakao.KakaoReissueParams;
-import com.groom.orbit.member.auth.app.oAuth.AuthToken;
+import com.groom.orbit.member.auth.app.dto.LoginResponseDto;
+import com.groom.orbit.member.auth.app.kakao.KakaoLoginParams;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
-public class AuthController {
+public class OAuthController {
 
   private final AuthService authService;
 
-  @PostMapping("/reissue")
-  public BaseResponse<AuthToken> reissue(@RequestBody KakaoReissueParams params) {
-    return BaseResponse.onSuccess(authService.reissue(params));
+  @PostMapping("/kakao")
+  public BaseResponse<LoginResponseDto> loginKakao(@RequestBody KakaoLoginParams params) {
+    return BaseResponse.onSuccess(authService.login(params));
   }
 }
